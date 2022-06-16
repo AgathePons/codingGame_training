1152952f727a570c3cb445e436018501cbf25f9a
AgathePons <ponsagathe37@gmail.com>
2022-06-16 16:27:39 +0200

    add exo

---
+++ match_number_to_another.js
@@ -0,0 +1,16 @@
+// Faire matcher un nombre donnée en entrée à un autre selon la correspondance suivante :
+// 0123456789 --> 9876543210
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+ const sort = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+ const sortReverse = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+ const N = parseInt(readline());
+ for (let i = 0; i < N; i++) {
+     const X = parseInt(readline());
+     console.error(sort.indexOf(X));
+     console.error(sortReverse[sort.indexOf(X)]);
+     console.log(sortReverse[sort.indexOf(X)]);
+ }
