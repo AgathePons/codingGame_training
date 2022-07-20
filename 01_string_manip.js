11311760a305bb9d7796a6698d4a7d8978e845fa
AgathePons <ponsagathe37@gmail.com>
2022-07-20 10:52:41 +0200

    add string methode file

---
+++ 01_string_manip.js
@@ -0,0 +1,36 @@
+const string = 'This is a simple string 123';
+
+// find with index
+const characterAtIndex = string.charAt(0) // index 0 of the string
+
+// split / join into array
+const splitedArray = string.split('');
+const joinedArray = splitedArray.join('');
+
+// slice / concat
+const firstPart = string.slice(0, 16);
+const secondPart = string.slice(-10, -1);
+
+const combined = firstPart.concat('add something', secondPart);
+
+// check regex
+const isMatching = string.match(/[a-z]/g); // true / false
+
+// UTF-16 / unicode code
+const characterCodeUTF = string.charCodeAt(0); // index 0 of the string
+const characterCodeUnicode = string.codePointAt(0); // index 0 of the string
+
+// uppercase / lowercase
+const lowerCaseString = string.toLowerCase();
+const upperCaseString = string.toUpperCase();
+
+// convert number / string
+const convertedNumber = Number(string);
+const convertedtoString = convertedNumber.toString();
+
+// replace
+const firstIByA = string.replace('i', 'a');
+const allIByA = string.replaceAll('i', 'a');
+
+// search in string
+const isSimpleIncluded = string.includes('simple'); // true / false
